153353bb84a64dc4b15ac4f238c55c36521ec7d5
stsztan <168595875+stsztan@users.noreply.github.com>
2024-05-10 21:37:48 +0200

    Update PlotlyStackComponent.jsx
    relative values instead of absolute values starting with 1.0 for the first element

--- src/PlotlyStackComponent.jsx
+++ src/PlotlyStackComponent.jsx
@@ -1,22 +1,24 @@
 import Plot from 'react-plotly.js';
 
 const PlotlyStackComponent = ({ timeseries_data }) => {
-    const timeseries_keys = Object.keys(timeseries_data)
+    const timeseries_keys = Object.keys(timeseries_data);
     const timeseries_absolute = timeseries_keys.map(e => parseInt(e));
     const data_keys = Object.keys(timeseries_data[timeseries_keys[0]]);
+    const min_ts_value = Math.min(...timeseries_absolute);
+    const timeseries_relative = timeseries_absolute.map(e => e - min_ts_value);
     const all_relative_series = data_keys.map(
         k => {
             return {
                 name: k,
-                x: timeseries_absolute,
+                x: timeseries_relative,
                 y: timeseries_keys.map(
-                    t => timeseries_data[t][k]
+                    (t, i) => timeseries_data[t][k] / timeseries_data[timeseries_keys[(i - 1) >= 0 ? (i - 1) : (0)]][k]
                 )
             }
         }
     );
 
-    return <Plot data={all_relative_series} />;
+    return <Plot data={all_relative_series.slice(0,500)} />;
 };
 
 export default PlotlyStackComponent;
